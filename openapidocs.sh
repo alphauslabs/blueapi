479cbe940f00f71ef37247853a538d4b510fb528
flowerinthenight <flowerinthenight@pm.me>
2021-02-10 21:12:08 +0900

    docs: add html

--- openapidocs.sh
+++
@@ -1,2 +0,0 @@
-protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true $(find ./blue -name '*.proto')
-protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true $(find ./awscost -name '*.proto')
