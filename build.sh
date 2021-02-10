a988545e499aafd3c0b2f239f8966e09fe0e4a37
flowerinthenight <flowerinthenight@pm.me>
2021-02-11 01:28:37 +0900

    feat: wip operations

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ echo "Compiling proto files..." && buf generate
 echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true $(find ./blue -name '*.proto')
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true $(find ./awscost -name '*.proto')
+protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true $(find ./operations -name '*.proto')
 
 echo "Generating html docs..."
 protoc --doc_out=./htmldocs --doc_opt=html,blue.html $(find ./blue -name '*.proto')
 protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./awscost -name '*.proto')
+protoc --doc_out=./htmldocs --doc_opt=html,operations.html $(find ./operations -name '*.proto')
