07d36aee1fd6847e6e8d351d557484e1e880d4af
flowerinthenight <flowerinthenight@pm.me>
2021-06-15 02:56:45 +0900

    chore(blue): merge cur to org

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true \
        ./org/v1/*.proto \
        ./iam/v1/*.proto \
-       ./curmgt/v1/*.proto \
        ./awscost/v1/*.proto \
        ./globalconfig/v1/*.proto \
        ./operations/v1/*.proto
@@ -12,7 +11,6 @@ protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --opena
 echo "Generating html docs..."
 protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./iam -name '*.proto')
 protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./org -name '*.proto')
-protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./curmgt -name '*.proto')
 protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./awscost -name '*.proto')
 protoc --doc_out=./htmldocs --doc_opt=html,operations.html $(find ./operations -name '*.proto')
 protoc --doc_out=./htmldocs --doc_opt=html,operations.html $(find ./globalconfig -name '*.proto')
