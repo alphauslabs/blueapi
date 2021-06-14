71a4cf5f0a4ddc5d88c279f18080c43f70f361da
flowerinthenight <flowerinthenight@pm.me>
2021-06-15 04:04:43 +0900

    chore(blue): make sure updated docs

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --opena
        ./globalconfig/v1/*.proto \
        ./operations/v1/*.proto
 
-echo "Generating html docs..."
-protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./iam -name '*.proto')
-protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./org -name '*.proto')
-protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./awscost -name '*.proto')
-protoc --doc_out=./htmldocs --doc_opt=html,operations.html $(find ./operations -name '*.proto')
-protoc --doc_out=./htmldocs --doc_opt=html,operations.html $(find ./globalconfig -name '*.proto')
+# echo "Generating html docs..."
+# protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./iam -name '*.proto')
+# protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./org -name '*.proto')
+# protoc --doc_out=./htmldocs --doc_opt=html,awscost.html $(find ./awscost -name '*.proto')
+# protoc --doc_out=./htmldocs --doc_opt=html,operations.html $(find ./operations -name '*.proto')
+# protoc --doc_out=./htmldocs --doc_opt=html,operations.html $(find ./globalconfig -name '*.proto')
