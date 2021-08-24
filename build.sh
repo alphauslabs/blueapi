d548010533f4f6b5f099c600856b24f16ac4ca1c
flowerinthenight <flowerinthenight@pm.me>
2021-08-24 14:08:53 +0900

    feat: add admin service

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true \
        ./org/v1/*.proto \
        ./iam/v1/*.proto \
+       ./admin/v1/*.proto \
        ./cost/v1/*.proto \
        ./billing/v1/*.proto \
        ./operations/v1/*.proto \
