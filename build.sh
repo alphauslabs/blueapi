c7c430d60a942851f7ae2ce42d5a4678c471f09c
flowerinthenight <flowerinthenight@pm.me>
2021-09-08 17:13:46 +0900

    chore: try python build

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 echo "Compiling proto files..." && buf generate
+python3 -m grpc_tools.protoc -I . --python_out=./generated/py --grpc_python_out=./generated/py \
+        ./org/v1/*.proto \
+        ./iam/v1/*.proto \
+        ./admin/v1/*.proto \
+        ./cost/v1/*.proto \
+        ./billing/v1/*.proto \
+        ./operations/v1/*.proto \
+        ./preferences/v1/*.proto
+        $(for v in $(find ./api -type d); do echo -n "$v/*.proto "; done)
 
 echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true \
